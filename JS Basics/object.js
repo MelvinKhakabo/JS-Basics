215222bd81db57aa30bc4177c716c1228fed6a10
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-03-12 12:20:31 +0300

    JS Basics

---
+++ JS Basics/object.js
@@ -0,0 +1,14 @@
+// Declaring objects in JS
+let person = {
+    name: 'John',
+    age: 30
+};
+console.log(person);
+
+//Changing values in objects
+//dot notation
+person.name = 'Mary';
+console.log(person.name);
+//bracket notation
+person['name'] = 'Mike';
+console.log(person.name);
