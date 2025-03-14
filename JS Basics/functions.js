e3af243eae298a2e6b810a4e7ca443ce38a351b6
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-03-14 11:06:09 +0300

    JS Basics

--- JS Basics/functions.js
+++ JS Basics/functions.js
@@ -1,1 +1,30 @@
-  
+//Writing funcions in JS
+function greet(){
+    console.log("Hello World!");
+} //doesn't need a semi-colon
+//call the function
+greet();
+
+//function with parameters
+function greet(name){
+    console.log("Hello " + name);
+}
+greet("John");
+greet("Mary");
+
+//function with multiple parameters
+function greet(name, lastName){
+    console.log("Hello " + name + " " + lastName);
+}
+greet("John", "Smith");
+greet("Mary", "Jane");
+
+
+//Other functions
+//function that calculates a value
+function square(number){
+    return number * number;
+}
+//call the function
+let number = square(2);
+console.log(number);    
