efb8604bd1e9aeb95032f45e06338f143861303d
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-03-16 14:39:42 +0300

    JS Basics

---
+++ First App/logger.js
@@ -0,0 +1,10 @@
+// JS tutorial
+// console.log('Hello World');
+// Compare this snippet from First%20App/logger.js: \
+// // JavaScript Basic Tutorial
+// console.log("Hello World!");
+// // JavaScript Basic Tutorial
+// console.log("Hello World!");
+// Compare this snippet from Test/logger.js:
+// // JavaScript Basic Tutorial
+// console.log("Hello World!");
