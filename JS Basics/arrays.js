10f1933a3cf8795e261a9ccd5aca99f1225f7f92
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-03-25 13:59:00 +0300

    JS Basics

--- JS Basics/arrays.js
+++ JS Basics/arrays.js
@@ -1,15 +1,1 @@
-// Working with arrays in JS. --- arrays are like lists in python
-//we use square brackets to define arrays[] and they can have differnt data types.
-let selectedColors = ['red', 'blue'];
-console.log(selectedColors);
-//use indexing to find elements in an array
-console.log(selectedColors[0]);
-console.log(selectedColors[1]);
-//add elements to an array -- add to the end of the array
-selectedColors[2] = 'green';
-console.log(selectedColors);
-//add elements to an array -- add to the beginning of the array
-selectedColors[0] = 'yellow';
-console.log(selectedColors);
-//length of an array
-console.log(selectedColors.length);
+
